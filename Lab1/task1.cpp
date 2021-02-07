76eada8e3024c69128e63bd2fe79474fba9b1643
Арсений Каплин <kaplin_arseny@mail.ru>
2021-02-07 14:11:20 +0300

    4 tasks added

---
+++ Lab1/task1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL,"ru");
+
+    const int pi = 3.14;
+    int a;
+    double x, f;
+
+    cout << "Введите 1, если надо использовать константы. Введите любое другое значение, если надо вводить значения." << endl;
+    cin >> a;
+    if (a == 1)
+    {
+        x = 1;
+        f = sin(pi / 2 + 3 * x) / (1 - sin(3 * x - pi));
+        cout << f;
+    }
+    else
+    {
+        cout << "Введите значение x" << endl;
+        cin >> x;
+        f = sin(pi / 2 + 3 * x) / (1 - sin(3 * x - pi));
+        cout << f;
+    }
+}
---
+++ Lab1/task2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(double x)
+{
+    const int pi = 3.14;
+    return sin(pi / 2 + 3 * x) / (1 - sin(3 * x - pi));
+}
+
+void main()
+{
+    setlocale(LC_ALL, "ru");
+    
+    int a;
+    double x;
+
+    cout << "¬ведите 1, если надо использовать константы. ¬ведите любое другое значение, если надо вводить значени€." << endl;
+    cin >> a;
+    if (a == 1)
+    {
+        x = 1;
+        cout << f(x);
+    }
+    else
+    {
+        cout << "¬ведите значение x" << endl;
+        cin >> x;
+        cout << f(x);
+    }
+}
